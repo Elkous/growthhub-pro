1487badb004ccd9e07f73840656e87ed502af70f
Elkous <114785266+Elkous@users.noreply.github.com>
2026-08-15 18:34:02 +0100

    Change Supabase publishable key
    Updated the Supabase key to a new publishable key.

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://pysqisdhivsrquaqefzp.supabase.co";
 
-const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY";
+const SUPABASE_KEY = "sb_publishable_YXgluHrlQJELPpeeD0nE2g_kjxE2cl-";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
